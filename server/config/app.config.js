b9e3514f7dbd814a38fb5fbb26d100afd4138597
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-12-13 03:38:46 -0500

    directory-frontend-4 Basic files required for the web project
    - Added initial files for the web project
    - Included SSL certificate for the web project
    - Created router and proxy for the web project
    - Enabled Vault integration for the web project
    - Included authentication app client for the backend services

---
+++ server/config/app.config.js
@@ -0,0 +1,124 @@
+/**
+ * Module for bootstrapping application configuration and handling secrets from Vault.
+ */
+
+let jobsProxyConfig = {};
+const fs = require('fs');
+const {format} = require('logform');
+const winston = require('winston');
+
+/**
+ * Bootstraps the application configuration by loading secrets from Vault if provided.
+ *
+ * @param {Object} app - The application instance.
+ * @returns {Promise} - A promise that resolves with the configuration or null.
+ */
+module.exports.bootstrapConfiguration = function (app) {
+    const argv = require('yargs').argv;
+
+    return new Promise(function (resolve, reject) {
+        require('dotenv').config({path: "default.env"});
+
+        if (argv.vaultToken && argv.vaultUrl && argv.vaultPath) {
+            loadSecretsIntoEnv(argv.vaultUrl, argv.vaultToken, argv.vaultPath, argv.debugMode).then(config => {
+                resolve(config);
+            }, error => {
+                logger.error("Unable to load secrets from vault");
+                reject(error);
+            });
+        } else {
+            logger.info("No vault configuration found");
+            resolve(null);
+        }
+    });
+};
+
+/**
+ * Loads secrets from Vault into environment variables.
+ *
+ * @param {string} vaultUrl - The URL of the Vault server.
+ * @param {string} vaultToken - The token for authenticating with Vault.
+ * @param {string} vaultPath - The path in Vault where secrets are stored.
+ * @param isDebugMode - A flag indicating whether debug mode is enabled.
+ * @returns {Promise} - A promise that resolves with the configuration or rejects with an error.
+ */
+let loadSecretsIntoEnv = function (vaultUrl, vaultToken, vaultPath, isDebugMode) {
+    return new Promise(function (resolve, reject) {
+        let vaultClient = require("node-vault-client");
+        console.log('[APP-CFG] - Vault token load:: OK');
+        if(isDebugMode) {
+            console.log(`[APP-CFG] - Vault token value:: ${vaultToken}`);
+        }
+
+        vaultClient = vaultClient.boot('main', {
+            api: {url: vaultUrl},
+            auth: {
+                type: 'token',
+                config: {token: vaultToken}
+            },
+        });
+
+        vaultClient.read(vaultPath).then(v => {
+            let vaultValues = v.__data.data;
+            for (let key in vaultValues) {
+                process.env[key] = vaultValues[key];
+            }
+            if (vaultValues !== undefined) {
+                logger.info("Loaded secrets from vault");
+                const config = require('config');
+                resolve(config);
+                if (isDebugMode) printVaultValues(vaultValues);
+            }
+        }).catch(e => {
+            logger.error("Unable to load secrets from vault", error);
+            reject("Unable to load secrets from vault", error);
+            console.error(e)
+        });
+    });
+};
+
+/**
+ * Creates a logger instance using Winston.
+ *
+ * @returns {Object} - A Winston logger instance.
+ */
+module.exports.getLoggerApp = function () {
+    return winston.createLogger({
+        format: format.combine(
+            format.json(),
+            format.timestamp(),
+        ),
+        transports: [
+            new winston.transports.Console()
+        ]
+    });
+};
+
+/**
+ * Reads and parses the jobs proxy configuration from a JSON file.
+ */
+module.exports.createDirectoryProxyConfig = function () {
+    const configJson = fs.readFileSync('server/config/config.json', 'utf8');
+    const config = JSON.parse(configJson);
+    jobsProxyConfig = config['directoryBackendProxyConfig'];
+    logger.info("[DS] - Proxy Config: " + JSON.stringify(jobsProxyConfig));
+};
+
+/**
+ * Retrieves the jobs proxy configuration.
+ *
+ * @returns {Object} - The jobs proxy configuration.
+ */
+module.exports.getJobsProxyConfig = function () {
+    return jobsProxyConfig;
+};
+
+let printVaultValues = function (vaultValues) {
+    logger.info("<><><><><><><><><><><><><><> Vault values <><><><><><><><><><><><><><>");
+    for (let key in vaultValues) {
+        logger.info(`${key}: ${vaultValues[key]}`);
+    }
+    logger.info("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
+}
+
+const logger = this.getLoggerApp();
---
+++ server/controller/directory-backend.controller.js
@@ -0,0 +1,277 @@
+/**
+ * OAuth client instance.
+ * @type {Object|null}
+ */
+let backboneClient = null;
+
+const appConfig = require('../config/app.config');
+const constants = require('../config/constants.util.js');
+const axios = require('axios');
+const jobsProxyConfig = appConfig.getJobsProxyConfig();
+const oauthclient = require('../proxy/oauth-client');
+const backboneclient = require('../proxy/backbone-client');
+const logger = appConfig.getLogger();
+const {v4: uuidv4} = require('uuid');
+const CryptoJS = require("crypto-js");
+
+const API_SERVICE_DIRECTORY_SESSION_RELATIVE_PATH = process.env.API_SERVICE_DIRECTORY_SESSION_RELATIVE_PATH;
+const API_SERVICE_DIRECTORY_MAP = JSON.parse(process.env.API_SERVICE_DIRECTORY_MAP);
+const OAUTH_AUTHENTICATION_TYPE = process.env.AUTH_AUTHENTICATION_TYPE;
+const OAUTH_CLIENT_ID = process.env.AUTH_CLIENT_ID;
+const OAUTH_CLIENT_SECRET = process.env.AUTH_CLIENT_SECRET;
+const OAUTH_GRANT_TYPE = process.env.AUTH_GRANT_TYPE;
+const OAUTH_TOKEN_URL = process.env.AUTH_SERVER_URI;
+const OAUTH_USER_ALIAS = process.env.AUTH_USER_ALIAS;
+const OAUTH_USER_PASSWORD = process.env.AUTH_USER_PASSWORD;
+
+const BACKBONE_API_SERVICE_MAP = JSON.parse(process.env.BACKBONE_API_SERVICE_MAP);
+const BACKBONE_OAUTH_AUTHENTICATION_TYPE = process.env.BACKBONE_AUTH_AUTHENTICATION_TYPE;
+const BACKBONE_OAUTH_CLIENT_ID = process.env.BACKBONE_AUTH_CLIENT_ID;
+const BACKBONE_OAUTH_CLIENT_SECRET = process.env.BACKBONE_AUTH_CLIENT_SECRET;
+const BACKBONE_OAUTH_GRANT_TYPE = process.env.BACKBONE_AUTH_GRANT_TYPE;
+const BACKBONE_OAUTH_TOKEN_URL = process.env.BACKBONE_AUTH_SERVER_URI;
+const BACKBONE_OAUTH_USER_ALIAS = process.env.BACKBONE_AUTH_USER_ALIAS;
+const BACKBONE_OAUTH_USER_PASSWORD = process.env.BACKBONE_AUTH_USER_PASSWORD;
+
+const HttpsAgent = require('agentkeepalive').HttpsAgent;
+const cKey = CryptoJS.enc.Utf8.parse(process.env.ENCRYPT_KEY);
+const iv = CryptoJS.enc.Utf8.parse(process.env.ENCRYPT_IV);
+
+const Agent = require('agentkeepalive');
+const {
+    AUTHORIZATION, BEARER, SESSION_TOKEN_BKD, FID_LOGGER_TRACKING_ID, CONTENT_TYPE, FID_USER_ID,
+    CONTENT_TYPE_DEFAULT, ACCEPT
+} = require("../config/constants.util");
+
+/**
+ * Jobs OAuth client configuration.
+ * @type {{password: string, clientId: string, tokenUrl: string, clientSecret: string, authenticationType: string, grantType: string, username: string}}
+ */
+const jobsOauthClientConfig = {
+    clientId: OAUTH_CLIENT_ID,
+    clientSecret: OAUTH_CLIENT_SECRET,
+    grantType: OAUTH_GRANT_TYPE,
+    tokenUrl: OAUTH_TOKEN_URL,
+    authenticationType: OAUTH_AUTHENTICATION_TYPE,
+    username: OAUTH_USER_ALIAS,
+    password: OAUTH_USER_PASSWORD
+};
+
+/**
+ * Backbone OAuth client configuration.
+ * @type {{password: string, clientId: string, tokenUrl: string, clientSecret: string, authenticationType: string, grantType: string, username: string}}
+ */
+const backboneOauthClientConfig = {
+    clientId: BACKBONE_OAUTH_CLIENT_ID,
+    clientSecret: BACKBONE_OAUTH_CLIENT_SECRET,
+    grantType: BACKBONE_OAUTH_GRANT_TYPE,
+    tokenUrl: BACKBONE_OAUTH_TOKEN_URL,
+    authenticationType: BACKBONE_OAUTH_AUTHENTICATION_TYPE,
+    username: BACKBONE_OAUTH_USER_ALIAS,
+    password: BACKBONE_OAUTH_USER_PASSWORD
+};
+
+/**
+ * HTTP connection options.
+ * @type {{maxFreeSockets: number, keepAlive: boolean, maxSockets: number, freeSocketTimeout: number, timeout: number}}
+ */
+const httpConnectionOptions = {
+    keepAlive: true,
+    maxSockets: 100,
+    maxFreeSockets: 10,
+    timeout: 60000,
+    freeSocketTimeout: 30000
+};
+
+/**
+ * Keepalive agent for HTTP connections.
+ * @type {AgentKeepAlive} keepaliveAgent - The keepalive agent.
+ */
+const keepaliveAgent = new Agent(httpConnectionOptions);
+
+/**
+ * Keepalive agent for HTTPS connections.
+ * @type {keepaliveHttpsAgent.HttpsAgent} keepaliveHttpsAgent
+ */
+const keepaliveHttpsAgent = new HttpsAgent(httpConnectionOptions);
+
+/**
+ * Retrieves the API endpoint for a given path.
+ *
+ * @param {string} path - The request path.
+ * @returns {string} - The full API endpoint URL.
+ * @throws {Error} - Throws an error if the API endpoint is not found.
+ */
+let getApiEndpoint = function (path) {
+    let finalPath = null;
+    let applicationName = null;
+    for (const element of jobsProxyConfig) {
+        if (element.matchOn != null && element.matchOn.startWith != null && path.startsWith(element.matchOn.startWith)) {
+            if (element.urlRewrite != null) {
+                finalPath = path.replace(element.urlRewrite.from, element.urlRewrite.to);
+            } else {
+                finalPath = path;
+            }
+            applicationName = element.applicationName;
+            break;
+        }
+    }
+    let apiURL = API_SERVICE_DIRECTORY_MAP[applicationName];
+
+    if (apiURL != null) {
+        return apiURL + finalPath;
+    } else {
+        throw errorUtil.createErrorResponse(constants.NOT_FOUND_REQUEST_CODE,
+            constants.NOT_FOUND_REQUEST_TITLE,
+            constants.NOT_FOUND_REQUEST_DETAIL,
+            constants.NOT_FOUND_REQUEST_CODE_VALUE);
+    }
+};
+
+/**
+ * Retrieves the backbone client instance, initializing it if necessary.
+ * @returns {Object} - The backbone client instance.
+ */
+let getBackboneClient = function () {
+    let backboneApiURL = BACKBONE_API_SERVICE_MAP['backbone'] + '/backbone/v1/session';
+    if (backboneClient) {
+        return backboneClient;
+    }
+    backboneClient = backboneclient.getBackbone({
+        url: backboneApiURL
+    });
+    return backboneClient;
+};
+
+/**
+ * Retrieves the OAuth client instance, initializing it if necessary.
+ *
+ * @returns {Object} - The OAuth client instance.
+ */
+let getOauthClient = function (oauthClientConfig) {
+    let oauthClient;
+    oauthClient = oauthclient.getOAuthClient(oauthClientConfig);
+    return oauthClient;
+};
+
+/**
+ * Retrieves the session token for the backbone service.
+ *
+ * @param req - The request object.
+ * @returns {Promise<*>} - The session token.
+ */
+const backboneSessionToken = async (req) => {
+    let backboneSession = null;
+    if (req.url === API_SERVICE_DIRECTORY_SESSION_RELATIVE_PATH) {
+        const backboneToken = await getOauthClient(backboneOauthClientConfig).getBearerToken();
+        backboneSession = await getBackboneClient().getToken(req.body.alias,
+            CryptoJS.AES.encrypt(req.body.password, cKey, {iv: iv}).toString(), backboneToken);
+    }
+    return backboneSession?.token;
+};
+
+/**
+ * Proxies API requests to the appropriate backend service.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
+const proxyApi = async (req, res, next) => {
+    let response = null;
+    try {
+        const apiURL = getApiEndpoint(req.url);
+        let jobsToken = await getOauthClient(jobsOauthClientConfig).getBearerToken();
+        let backboneSession = await backboneSessionToken(req);
+        let headers = getRequestHeader(req, jobsToken, backboneSession, constants.CONTENT_TYPE_DEFAULT, constants.CONTENT_TYPE_DEFAULT);
+        // Trace
+        logger.info(`[DIS] Proxying request to ${apiURL}`);
+        let httpOptions = createRequestOption(req.method, apiURL, req.body, headers);
+        let axiosResponse = await axios(httpOptions);
+
+        delete axiosResponse.headers['transfer-encoding'];
+        response = axiosResponse.data;
+        res.set(axiosResponse.headers);
+
+        // Include the session-token-bkd in the response headers
+        if (backboneSession && req.url === API_SERVICE_DIRECTORY_SESSION_RELATIVE_PATH) {
+            res.set(SESSION_TOKEN_BKD, backboneSession);
+        }
+    } catch (error) {
+        if (error.response != null) {
+            response = error.response.data;
+            res.status(error.response.status);
+        } else if (error.errors != null) {
+            response = error;
+            res.status(500);
+        }
+    }
+    res.send(response);
+};
+
+/**
+ * Constructs the basic headers for the proxied request.
+ *
+ * @param {Object} req - The request object.
+ * @param jobsToken - The token for the jobs service.
+ * @param backboneSession - The session token for the backbone service.
+ * @param {string} defaultAccept - The default Accept header value.
+ * @param {string} defaultContentType - The default Content-Type header value.
+ * @returns {Object} - The constructed headers.
+ */
+const getRequestHeader = function (req, jobsToken, backboneSession, defaultAccept, defaultContentType) {
+    let headers = getBasicHeader(req, jobsToken, backboneSession, defaultAccept);
+    headers[CONTENT_TYPE] = req.header(CONTENT_TYPE) == null ? defaultContentType : req.header(CONTENT_TYPE);
+    return headers;
+};
+
+/**
+ * Constructs the basic headers for the proxied request.
+ * @param req - The request object.
+ * @param jobsToken - The token for the jobs service.
+ * @param backboneSession - The session token for the backbone service.
+ * @param defaultAccept - The default Accept header value.
+ * @returns {{}} - The constructed headers.
+ */
+const getBasicHeader = function (req, jobsToken, backboneSession, defaultAccept) {
+    let headers = {};
+    headers[FID_LOGGER_TRACKING_ID] = req.header(FID_LOGGER_TRACKING_ID) == null ? uuidv4() : req.header(FID_LOGGER_TRACKING_ID);
+    headers[FID_USER_ID] = req.header(FID_USER_ID) == null ? "anonymous" : req.header(FID_USER_ID);
+    headers[AUTHORIZATION] = BEARER + jobsToken;
+    headers[ACCEPT] = req.header(ACCEPT) == null ? defaultAccept : req.header(ACCEPT);
+
+    if (req.header(SESSION_TOKEN_BKD) !== null) {
+        headers[SESSION_TOKEN_BKD] = req.header(SESSION_TOKEN_BKD);
+    }
+
+    if (backboneSession) {
+        headers[SESSION_TOKEN_BKD] = backboneSession;
+    }
+
+    return headers;
+};
+
+/**
+ * Creates the request options for the proxied request.
+ *
+ * @param {string} method - The HTTP method.
+ * @param {string} url - The request URL.
+ * @param {Object} body - The request body.
+ * @param {Object} headers - The request headers.
+ * @returns {Object} - The constructed request options.
+ */
+let createRequestOption = function (method, url, body, headers) {
+    return {
+        method: method.toLowerCase(),
+        url: url,
+        data: body != null ? body : null,
+        headers: headers,
+        httpAgent: keepaliveAgent,
+        httpsAgent: keepaliveHttpsAgent,
+        responseType: headers[ACCEPT] === CONTENT_TYPE_DEFAULT ? 'blob' : 'json'
+    }
+};
+
+module.exports = {
+    proxyApi
+};
